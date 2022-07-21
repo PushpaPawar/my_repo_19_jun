301ff741577a0d2df2e1cf38d44f6c6dbfe3878a
pushpa rathod <pushpa.rathod@cloudreach.com>
2022-07-21 15:00:14 +0530

    missing routes

--- route.tf
+++ route.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "nat_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
+    gateway_id = aws_nat_gateway.nat_route_table.id
   }
 
   

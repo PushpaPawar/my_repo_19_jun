a3b64f7d408beca5aa0b6d9c763910441e19e33a
pushpa rathod <pushpa.rathod@cloudreach.com>
2022-07-21 14:03:22 +0530

    route table

--- subnet.tf
+++ subnet.tf
@@ -1,12 +1,12 @@
 resource "aws_route_table_association" "Public" {
   subnet_id      = aws_subnet.Public.id
-  route_table_id = aws_route_table.IG.id
+  route_table_id = aws_route_table.gw.id
 }
 resource "aws_route_table_association" "Private" {
   subnet_id      = aws_subnet.Private.id
-  route_table_id = aws_route_table.nat_route.id
+  route_table_id = aws_route_table.nat_gw.id
 }
 resource "aws_route_table_association" "Data" {
   subnet_id      = aws_subnet.Data.id
-  route_table_id = aws_route_table.nat_route.id
+  route_table_id = aws_route_table.nat_gw.id
 }
